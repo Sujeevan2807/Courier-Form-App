d6ffb7ce92427ba82e4ebde74647ef81ae6f0ed8
Sujeevan B <152384304+Sujeevan2807@users.noreply.github.com>
2024-07-17 15:58:55 +0530

    Fixed : Date Format (#2)

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,105 +1,118 @@
-import JustValidate from 'just-validate';
-import { rule } from 'postcss';
+import JustValidate from "just-validate";
+import { rule } from "postcss";
+import * as myDay from "dayjs";
+import dayjs from "dayjs";
+import {formatMyDate} from "./utils"
 
-const formEl = document.querySelector("#courier-form")
+const formEl = document.querySelector("#courier-form");
 
-const localStorageKey = "Courier Form Data"
+const localStorageKey = "Courier Form Data";
 
-const validate = new JustValidate('#courier-form',{
-    validateBeforeSubmitting: true,
-  });
-
-
-validate
-.addField("#nameInput",[
-    {
-        rule : "required",
-    },
-    {
-        rule : "minLength",
-        value : 3,
-    },
-    {
-        rule : "maxLength",
-        value : 20,
-    },
-], {
-    errorLabelCssClass: ['errorLableStyle'],
-    errorFieldCssClass: ['errorFieldStyle']
-})
-.addField("#numberInput",[
-    {
-        rule : "required",
-    },
-    {
-        rule : "number",
-    },
-], {
-    errorLabelCssClass: ['errorLableStyle'],
-    errorFieldCssClass: ['errorFieldStyle']
-})
-.addField("#dateInput",[
-    {
-        rule : "required",
-    },
-    
-], {
-    errorLabelCssClass: ['errorLableStyle'],
-    errorFieldCssClass: ['errorFieldStyle']
-})
-.addField("#addressInput",[
-    {
-        rule : "required",
-    },
-    
-], {
-    errorLabelCssClass: ['errorLableStyle'],
-    errorFieldCssClass: ['errorFieldStyle']
-})
-
-
-validate.onSuccess(() => {
-    const formData = new FormData(formEl)
-
-    const formEntriesData = Object.fromEntries(formData.entries());
-
-    const formEntryDataArray = []
-
-    const getLocalStorageData = localStorage.getItem(localStorageKey)
-
-    const formEntryDataObj = JSON.parse(getLocalStorageData)
-
-    if(formEntryDataObj){
-        formEntryDataObj.push(formEntriesData)
-        localStorage.setItem(localStorageKey, JSON.stringify(formEntryDataObj))
-    }else{
-        formEntryDataArray.push(formEntriesData)
-        localStorage.setItem(localStorageKey, JSON.stringify(formEntryDataArray))
-    }
-    alert("Form submited succesfully")
-    formEl.reset()
-   
+const validate = new JustValidate("#courier-form", {
+  validateBeforeSubmitting: true,
 });
 
+validate
+  .addField(
+    "#nameInput",
+    [
+      {
+        rule: "required",
+      },
+      {
+        rule: "minLength",
+        value: 3,
+      },
+      {
+        rule: "maxLength",
+        value: 20,
+      },
+    ],
+    {
+      errorLabelCssClass: ["errorLableStyle"],
+      errorFieldCssClass: ["errorFieldStyle"],
+    }
+  )
+  .addField(
+    "#numberInput",
+    [
+      {
+        rule: "required",
+      },
+      {
+        rule: "number",
+      },
+    ],
+    {
+      errorLabelCssClass: ["errorLableStyle"],
+      errorFieldCssClass: ["errorFieldStyle"],
+    }
+  )
+  .addField(
+    "#dateInput",
+    [
+      {
+        rule: "required",
+      },
+    ],
+    {
+      errorLabelCssClass: ["errorLableStyle"],
+      errorFieldCssClass: ["errorFieldStyle"],
+    }
+  )
+  .addField(
+    "#addressInput",
+    [
+      {
+        rule: "required",
+      },
+    ],
+    {
+      errorLabelCssClass: ["errorLableStyle"],
+      errorFieldCssClass: ["errorFieldStyle"],
+    }
+  );
+
+validate.onSuccess(() => {
+  const formData = new FormData(formEl);
+
+  const formEntriesData = Object.fromEntries(formData.entries());
+
+  const formEntryDataArray = [];
+
+  const getLocalStorageData = localStorage.getItem(localStorageKey);
+
+  const formEntryDataObj = JSON.parse(getLocalStorageData);
+
+  if (formEntryDataObj) {
+    formEntryDataObj.push(formEntriesData);
+    localStorage.setItem(localStorageKey, JSON.stringify(formEntryDataObj));
+  } else {
+    formEntryDataArray.push(formEntriesData);
+    localStorage.setItem(localStorageKey, JSON.stringify(formEntryDataArray));
+  }
+  alert("Form submited succesfully");
+  formEl.reset();
+});
 
 function getAllCourierData() {
-    const courierData = localStorage.getItem(localStorageKey)
+  const courierData = localStorage.getItem(localStorageKey);
 
-    const courierDataArr = JSON.parse(courierData)
+  const courierDataArr = JSON.parse(courierData);
 
-    if(courierDataArr){
+  if (courierDataArr) {
+    const tableCardEl = document.querySelector("#tableCard");
 
-        const tableCardEl = document.querySelector("#tableCard")
+    tableCardEl.classList.remove("hidden");
+    const dataTableEl = document.querySelector("#courier-data-table");
 
-        tableCardEl.classList.remove("hidden")
-        const dataTableEl = document.querySelector("#courier-data-table")
-
-        const finalData = courierDataArr.map((courierData) => {
-            return `
+    const finalData = courierDataArr
+      .map((courierData) => {
+        return `
             <tr>
                 <td class="table-row-style">${courierData.name}</td>
                 <td class="table-row-style">${courierData.number}</td>
-                <td class="table-row-style">${courierData.date}</td>
+                <td class="table-row-style">${formatMyDate(courierData.date)}</td>
                 <td class="table-row-style">${courierData.address}</td>
                 <td class="table-row-style">
                     <button
@@ -108,12 +121,14 @@ function getAllCourierData() {
                     </button>
                 </td>
             </tr>
-            `
-        }).join(" ")
-        dataTableEl.innerHTML += finalData;
-    }else{
-        console.log("No Values in Local Storage");
-    }
+            `;
+      })
+      .join(" ");
+    dataTableEl.innerHTML += finalData;
+  } else {
+    console.log("No Values in Local Storage");
+  }
 }
 
-getAllCourierData()
+getAllCourierData();
+
---
+++ assets/js/utils.js
@@ -0,0 +1,6 @@
+import dayjs from "dayjs";
+
+export function formatMyDate(date, format = "DD, MMM-YYYY, hh-mma") {
+    return dayjs(date).format(format);
+   }
+   
